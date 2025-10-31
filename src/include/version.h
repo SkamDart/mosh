3183727a48fc7862fe71e71e264eaf686c20cf19
Cameron Dart <8763518+SkamDart@users.noreply.github.com>
2025-10-31 00:34:48 -0700

    autoconf artifacts

---
+++ src/include/version.h
@@ -0,0 +1,9 @@
+/* version.h - Build version information */
+/* This file replaces the autoconf-generated version.h */
+
+#ifndef VERSION_H
+#define VERSION_H
+
+#define BUILD_VERSION "zig-20251031"
+
+#endif /* VERSION_H */
